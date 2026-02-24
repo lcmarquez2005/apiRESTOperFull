7004e748323bcc4d5587511ad48334327264c127
80chucho08 <chucho2004carbajal@gmail.com>
2026-02-24 14:14:05 -0600

    controller

--- apiRESTOperFull/Controllers/operacionesController.cs
+++ apiRESTOperFull/Controllers/operacionesController.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using apiRESTOperFull.Models;
 
 namespace apiRESTOperFull.Controllers
 {
     public class operacionesController : ApiController
     {
+        public static clsOperaciones[] objOperaciones = null; 
+
+
     }
 }
