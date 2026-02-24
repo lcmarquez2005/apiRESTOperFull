d27e1ea1fbaadc1538d5378ec08e99a438736ce2
80chucho08 <chucho2004carbajal@gmail.com>
2026-02-24 13:57:40 -0600

    variables en modelo

--- apiRESTOperFull/Models/clsOperaciones.cs
+++ apiRESTOperFull/Models/clsOperaciones.cs
@@ -7,5 +7,15 @@ namespace apiRESTOperFull.Models
 {
     public class clsOperaciones
     {
+        // Definicion de atributos
+        public double oper1 { get; set; }
+        
+        public double oper2 { get; set; }
+
+        public double oper3 { get; set; }
+        
+        public double resul { get; set; }
+
+        public string calculo { get; set; }
     }
 }
